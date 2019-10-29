9bbca740db498043fd6c182a4ab4f4942c597f7e
Nckflannery <Nckflannery@users.noreply.github.com>
2019-10-29 12:42:56 -0700

    Pep8 format

--- functions/sample_df.py
+++ functions/sample_df.py
@@ -1,7 +1,7 @@
 import pandas as pd
 columns = ['Year', 'Name', 'YearName', 'College', 'POS', 'Height',
-       'Weight', 'Hand Size', 'Arm Length', 'Wonderlic',
-       '40 Yard', 'Bench Press', 'Vert Leap', 'Broad Jump',
-       'Shuttle', '3Cone', '60Yd Shuttle', 'Pick', 'Round']
-
-sample_df = pd.read_csv(r'C:\Users\Nick\Unit2Project\Attempt 2\Data\NFL Combine Data - Combine Results.csv', names=columns).drop(0)
+           'Weight', 'Hand Size', 'Arm Length', 'Wonderlic',
+           '40 Yard', 'Bench Press', 'Vert Leap', 'Broad Jump',
+           'Shuttle', '3Cone', '60Yd Shuttle', 'Pick', 'Round']
+datapath = r'C:\Users\Nick\Unit2Project\Attempt 2\Data'
+df = pd.read_csv(datapath + r'\NFLCombineData.csv', names=columns).drop(0)
