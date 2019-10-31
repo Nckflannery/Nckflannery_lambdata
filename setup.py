5f56fc6bbb11a57b591b31d889f470a52e26a5d1
Nckflannery <Nckflannery@users.noreply.github.com>
2019-10-30 17:34:10 -0700

    Update Version

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="Nckflannery_lambdata",
-    version = "0.2.25",
+    version = "0.2.30",
     author = "Nckflannery",
     description = "A collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://nckflannery.github.io/",
     packages=setuptools.find_packages(),
+    package_data={'':['*.csv']},
     python_requires=">=3.5",
     install_requires = REQUIRED,
     include_package_data=True,
